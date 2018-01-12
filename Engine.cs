43a920dcfcd2e342b936b7aa5be8ac9f8261e7ca
Jim Sample <gjsample3@gmail.com>
2018-01-12 14:45:33 -0500

    Revamping engine for more complacated simulation

--- Engine.cs
+++ Engine.cs
@@ -45,27 +45,98 @@ namespace Dots
                     yield return new Position(this.X - 1, this.Y - 1);
                 }
             }
+            public IEnumerable<Position> Nearby
+            {
+                get
+                {
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        for (int y = -1; y <= 1; y++)
+                        {
+                            if (x != 0 || y != 0) yield return new Position(this.X + x, this.Y + y);
+                        }
+                    }
+                }
+            }
+            public IEnumerable<Position> Inrange(int distance)
+            {
+                for (int x = -distance; x <= distance; x++)
+                {
+                    for (int y = -distance; y <= distance; y++)
+                    {
+                        var scanPosition = new Position(this.X + x, this.Y + y);
+                        if (this.Distance(scanPosition) <= distance) yield return scanPosition;
+                    }
+                }
+            }
+            public double Distance(Position position)
+            {
+                return Math.Sqrt(Math.Pow(((double)position.X - (double)this.X), 2) + Math.Pow(((double)position.Y - (double)this.Y), 2));
+            }
         }
+
+        public enum DotTypes { City, Unit }
 
         public sealed class Dot
         {
+            public Engine Engine { get; set; }
             public Position Position { get; set; }
-            public int Owner { get; set; }
-            public int Hits { get; set; }
-            public int Resources { get; set; }
+            public Empire Empire { get; set; }
+            public DotTypes Type { get; set; }
+
             public int Tier { get; set; }
-            public int Disappointment { get; set; }
+            public int Hits { get; set; }
+            public int MaxHits { get { return this.Tier * 10; } }
+            public int ControlRange
+            {
+                get
+                {
+                    if (this.Type == DotTypes.City) return 2;
+                    else return 1;
+                }
+            }
+
+            public Dot(Engine engine, Empire empire, Position position, DotTypes type)
+            {
+                this.Engine = engine;
+                this.Empire = empire;
+                this.Position = position;
+                this.Type = type;
+                this.Tier = 1;
+                this.Hits = this.MaxHits;
+            }
+        }
+
+        public sealed class Empire
+        {
+            public Engine Engine { get; set; }
+            public int Id { get; set; }
+            public int Resources { get; set; }
+            public HashSet<Dot> Dots { get; set; }
+            public HashSet<Position> Tiles { get; set; }
+
+            public Empire(Engine engine, int id)
+            {
+                this.Engine = engine;
+                this.Id = id;
+                this.Resources = 0;
+                this.Dots = new HashSet<Dot>();
+                this.Tiles = new HashSet<Position>();
+            }
+
         }
 
         private object ThreadLock = new object();
-        private Dictionary<Position, int> OwnerByPosition;
         private Dictionary<Position, int> ChangesSinceLastRender;
-        private List<Dot> Dots;
+        private HashSet<Dot> Dots;
+        private HashSet<Empire> Empires;
         private int MapWidth;
         private int MapHeight;
         private Random Rng;
         private bool StopRequested;
         private bool isEngineRunning;
+        private Dictionary<Position, Empire> ClaimByPosition;
+        private Dictionary<Position, HashSet<Dot>> ClaimantsByPosition;
 
         public bool IsEngineRunning
         {
@@ -80,40 +151,76 @@ namespace Dots
             }
         }
 
-        public Engine(int startingDotCount, int mapWidth, int mapHeight)
+        public Engine(int startingCount, int mapWidth, int mapHeight)
         {
             lock (this.ThreadLock)
             {
-                this.OwnerByPosition = new Dictionary<Position, int>();
-                this.ChangesSinceLastRender = new Dictionary<Position, int>();
-                this.Dots = new List<Dot>();
-                this.MapHeight = mapHeight;
-                this.MapWidth = mapWidth;
-                this.Rng = new Random();
-
-                for (int i = 0; i < startingDotCount; i++)
-                {
-                    var dot = new Dot();
-                    dot.Owner = i + 1;
-                    var position = new Position(this.Rng.Next(this.MapWidth + 1), this.Rng.Next(this.MapHeight + 1));
-                    while (this.OwnerByPosition.ContainsKey(position))
-                    {
-                        position = new Position(this.Rng.Next(this.MapWidth + 1), this.Rng.Next(this.MapHeight + 1));
-                    }
-                    dot.Position = position;
-                    this.Dots.Add(dot);
-                    this.MoveDotTo(dot, position);
-                }
+                this.Initialize(mapWidth, mapHeight);
+                this.SpawnInitialEmpires(startingCount);
             }
         }
 
+        private void SpawnInitialEmpires(int startingCount)
+        {
+            var spawnPoints = new HashSet<Position>();
+            for (int x = 0; x < this.MapWidth; x++)
+            {
+                for (int y = 0; y < this.MapWidth; y++)
+                {
+                    spawnPoints.Add(new Position(x, y));
+                }
+            }
+
+            for (int i = 0; i < startingCount; i++)
+            {
+                if (spawnPoints.Count == 0) break;
+                var spawnPoint = spawnPoints.ToList()[this.Rng.Next(spawnPoints.Count)];
+                foreach (var point in spawnPoint.Inrange(5))
+                {
+                    spawnPoints.Remove(point);
+                }
+
+                var empire = new Empire(this, i + 1);
+                this.AddEmpire(empire);
+                var dot = new Dot(this, empire, spawnPoint, DotTypes.City);
+                this.AddDot(dot);
+            }
+        }
+        private void Initialize(int mapWidth, int mapHeight)
+        {
+            this.ChangesSinceLastRender = new Dictionary<Position, int>();
+            this.Dots = new HashSet<Dot>();
+            this.Empires = new HashSet<Empire>();
+            this.MapHeight = mapHeight;
+            this.MapWidth = mapWidth;
+            this.Rng = new Random();
+            this.ClaimantsByPosition = new Dictionary<Position, HashSet<Dot>>();
+            this.ClaimByPosition = new Dictionary<Position, Empire>();
+        }
+        private void AddDot(Dot dot)
+        {
+            this.Dots.Add(dot);
+            dot.Empire.Dots.Add(dot);
+            this.MoveDotTo(dot, dot.Position);
+        }
+        private void AddEmpire(Empire empire)
+        {
+            this.Empires.Add(empire);
+        }
         private void MoveDotTo(Dot dot, Position position)
         {
+            this.RemoveDotFrom(dot, dot.Position);
+
             dot.Position = position;
-            if (!this.OwnerByPosition.ContainsKey(position)) this.OwnerByPosition.Add(position, 0);
-            if (this.OwnerByPosition[position] != dot.Owner)
+            foreach (var tile in position.Inrange(dot.ControlRange))
             {
-                this.OwnerByPosition[position] = dot.Owner;
+                this.ClaimTile(tile, dot);
+            }
+
+            if (!this.OwnerByPosition.ContainsKey(position)) this.OwnerByPosition.Add(position, 0);
+            if (this.OwnerByPosition[position] != dot.Empire)
+            {
+                this.OwnerByPosition[position] = dot.Empire;
 
                 dot.Resources += 1;
                 if (dot.Resources >= (dot.Tier * 10))
@@ -128,29 +235,49 @@ namespace Dots
 
             }
             if (!this.ChangesSinceLastRender.ContainsKey(position)) this.ChangesSinceLastRender.Add(position, 0);
-            this.ChangesSinceLastRender[position] = dot.Owner;
+            this.ChangesSinceLastRender[position] = dot.Empire;
         }
 
-        public void Stop()
+        private void RemoveDotFrom(Dot dot, Position position)
         {
+            foreach (var tile in dot.Position.Inrange(dot.ControlRange))
+            {
+                this.RemoveTileClaim(tile, dot);
+            }
+        }
+
+        private void RemoveTileClaim(Position tile, Dot dot)
+        {
+            if (!this.ClaimantsByPosition.ContainsKey(tile)) this.ClaimantsByPosition.Add(tile, new HashSet<Dot>());
+            this.ClaimantsByPosition[tile].Remove(dot);
+            if (!this.ClaimByPosition.ContainsKey(tile)) this.ClaimByPosition.Add(tile, null);
+            var claimants = this.ClaimantsByPosition[tile].Select(o => o.Empire).Distinct();
+            if (this.ClaimByPosition[tile] != null)
+            {
+
+            }
+            else
+            {
+            }
+        }
+
+        private void ClaimTile(Position tile, Dot dot)
+        {
+            throw new NotImplementedException();
+        }
+        private bool InBounds(Position position)
+        {
+            var result = true;
             lock (this.ThreadLock)
             {
-                this.StopRequested = true;
+                if (position.X < 0) result = false;
+                if (position.X > this.MapWidth) result = false;
+                if (position.Y < 0) result = false;
+                if (position.Y > this.MapHeight) result = false;
             }
+            return result;
         }
-        public void Start()
-        {
-            if (!this.IsEngineRunning)
-            {
-                lock (this.ThreadLock)
-                {
-                    this.StopRequested = false;
-                    this.isEngineRunning = true;
-                    ThreadPool.QueueUserWorkItem(this.Spin);
-                }
-            }
-        }
-        public void Spin(object threadArguments)
+        private void Spin(object threadArguments)
         {
             var continueSpinning = false;
             lock (this.ThreadLock) { continueSpinning = !this.StopRequested; }
@@ -175,19 +302,6 @@ namespace Dots
             }
         }
 
-        private bool InBounds(Position position)
-        {
-            var result = true;
-            lock (this.ThreadLock)
-            {
-                if (position.X < 0) result = false;
-                if (position.X > this.MapWidth) result = false;
-                if (position.Y < 0) result = false;
-                if (position.Y > this.MapHeight) result = false;
-            }
-            return result;
-        }
-
         public List<KeyValuePair<Position, int>> GetRenderChanges()
         {
             List<KeyValuePair<Position, int>> result;
@@ -199,6 +313,25 @@ namespace Dots
             }
 
             return result;
+        }
+        public void Stop()
+        {
+            lock (this.ThreadLock)
+            {
+                this.StopRequested = true;
+            }
+        }
+        public void Start()
+        {
+            if (!this.IsEngineRunning)
+            {
+                lock (this.ThreadLock)
+                {
+                    this.StopRequested = false;
+                    this.isEngineRunning = true;
+                    ThreadPool.QueueUserWorkItem(this.Spin);
+                }
+            }
         }
     }
 }
